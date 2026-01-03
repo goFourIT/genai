b8509bef5a2292f61205567e678f6249d9ad154e
Cezar <biculescu.cezar@gmail.com>
2026-01-03 18:59:31 +0200

    project init

---
+++ src/main/java/io/github/go4it/semanticsearch/SemanticsearchApplication.java
@@ -0,0 +1,13 @@
+package io.github.go4it.semanticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/go4it/semanticsearch/SemanticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.go4it.semanticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
