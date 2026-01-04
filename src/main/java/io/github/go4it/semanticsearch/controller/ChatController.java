128e3eced754ace178ac973f25ea573b38295d0f
Cezar <biculescu.cezar@gmail.com>
2026-01-04 19:05:59 +0200

    create ChatController, ChatService

--- src/main/java/io/github/go4it/semanticsearch/controller/ChatController.java
+++ src/main/java/io/github/go4it/semanticsearch/controller/ChatController.java
@@ -21,4 +21,9 @@ public class ChatController {
     ResponseEntity<String> sendMessage(@RequestBody String message) {
         return ResponseEntity.ok(this.chatService.sendMessage(message));
     }
+
+    @PostMapping("/send-business-message")
+    ResponseEntity<String> sendBusinessMessage(@RequestBody String message) {
+        return ResponseEntity.ok(this.chatService.sendBusinessMessage(message));
+    }
 }
--- src/main/java/io/github/go4it/semanticsearch/service/ChatService.java
+++ src/main/java/io/github/go4it/semanticsearch/service/ChatService.java
@@ -1,6 +1,8 @@
 package io.github.go4it.semanticsearch.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,12 +10,23 @@ public class ChatService {
 
     private final ChatClient chatClient;
 
-    public ChatService(ChatClient chatClient) {
+    private final VectorStore vectorStore;
+
+    public ChatService(ChatClient chatClient, VectorStore vectorStore) {
         this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
     }
 
     public String sendMessage(String message) {
         return this.chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+    public String sendBusinessMessage(String message) {
+        return this.chatClient.prompt()
+                .advisors(QuestionAnswerAdvisor.builder(vectorStore).build())
                 .user(message)
                 .call()
                 .content();
