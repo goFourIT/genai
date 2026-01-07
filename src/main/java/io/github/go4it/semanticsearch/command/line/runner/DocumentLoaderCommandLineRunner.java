0c4f542ac9ffae488a45e31411365182683b7b53
Cezar <biculescu.cezar@gmail.com>
2026-01-07 10:34:39 +0200

    modify vector database loader

--- src/main/java/io/github/go4it/semanticsearch/command/line/runner/DocumentLoaderCommandLineRunner.java
+++ src/main/java/io/github/go4it/semanticsearch/command/line/runner/DocumentLoaderCommandLineRunner.java
@@ -1,6 +1,11 @@
 package io.github.go4it.semanticsearch.command.line.runner;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.go4it.semanticsearch.domain.Book;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.EmptyJsonMetadataGenerator;
+import org.springframework.ai.reader.JsonMetadataGenerator;
 import org.springframework.ai.reader.JsonReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -10,30 +15,37 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class DocumentLoaderCommandLineRunner implements CommandLineRunner {
 
     private final VectorStore vectorStore;
     private final Resource booksFile;
+    private final ObjectMapper objectMapper;
 
 
     public DocumentLoaderCommandLineRunner(
             VectorStore vectorStore,
-            @Value("classpath:books.json") Resource booksFile
+            @Value("classpath:books.json") Resource booksFile,
+            ObjectMapper objectMapper
     ) {
         this.vectorStore = vectorStore;
         this.booksFile = booksFile;
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        JsonReader jsonReader = new JsonReader(this.booksFile, "title", "author", "numberOfPages", "description");
-        List<Document> documents = jsonReader.get();
-
-        TokenTextSplitter tokenTextSplitter = new TokenTextSplitter();
-        List<Document> transformedDocuments = tokenTextSplitter.apply(documents);
-
-        vectorStore.add(transformedDocuments);
+//        JsonReader jsonReader = new JsonReader(this.booksFile, "description");
+//        List<Document> documents = jsonReader.get();
+        List<Book> books = this.objectMapper.readValue(this.booksFile.getInputStream(), new TypeReference<List<Book>>() {});
+        List<Document> documents = books.stream().map(book -> new Document(
+                "Book titled " + book.getTitle() + " written by " + book.getAuthor() + " having " + book.getNumberOfPages() + " pages.\n" + book.getDescription(),
+                Map.of("title", book.getTitle(),
+                        "author", book.getAuthor(),
+                        "numberOfPages", book.getNumberOfPages())
+        )).toList();
+//        vectorStore.add(documents);
     }
 }
---
+++ src/main/java/io/github/go4it/semanticsearch/domain/Book.java
@@ -0,0 +1,40 @@
+package io.github.go4it.semanticsearch.domain;
+
+public class Book {
+    private String title;
+    private String author;
+    private Integer numberOfPages;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
