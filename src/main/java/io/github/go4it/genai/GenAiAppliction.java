52be5fa3e91bf9e076f9c9598272cd63eff33c78
Cezar <biculescu.cezar@gmail.com>
2026-02-13 16:20:13 +0200

    change name to GenAiApplication from SemanticsearchApplication

--- src/main/java/io/github/go4it/genai/GenAiAppliction.java
+++ src/main/java/io/github/go4it/genai/GenAiAppliction.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SemanticsearchApplication {
+public class GenAiAppliction {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SemanticsearchApplication.class, args);
+		SpringApplication.run(GenAiAppliction.class, args);
 	}
 
 }
